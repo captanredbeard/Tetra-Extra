231f6d9925756fdc3bafe5897850c5bcf34c05a3
captanredbeard <captanredbeard2@gmail.com>
2021-09-15 19:01:26 -0500

    Attemped to setup a modular item. failed. feel free to take a look at it.

--- src/main/java/mod/captanredbeard/tetra_extra/items/modular/impl/ModularGloveItem.java
+++ src/main/java/mod/captanredbeard/tetra_extra/items/modular/impl/ModularGloveItem.java
@@ -1,25 +1,16 @@
 package mod.captanredbeard.tetra_extra.items.modular.impl;
 
-import net.minecraft.client.resources.I18n;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.registries.ObjectHolder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import se.mickelus.tetra.effect.ChargedAbilityEffect;
 import se.mickelus.tetra.gui.GuiModuleOffsets;
 import se.mickelus.tetra.items.TetraItemGroup;
 import se.mickelus.tetra.items.modular.ItemModularHandheld;
-import se.mickelus.tetra.module.SchematicRegistry;
-import se.mickelus.tetra.module.schematic.RemoveSchematic;
-import se.mickelus.tetra.module.schematic.RepairSchematic;
 
-import java.util.Arrays;
-import java.util.Optional;
 
-public class ModularGloveItem extends ItemModularHandheld {
+public class ModularGloveItem extends ItemModularHandheld{
     private static final Logger logger = LogManager.getLogger();
     public static final String bracerKey = "glove/bracer";
     public static final String wristKey = "glove/wrist";
@@ -31,7 +22,7 @@ public class ModularGloveItem extends ItemModularHandheld {
     private static final GuiModuleOffsets majorOffsets = new GuiModuleOffsets(new int[]{-13, -1, 3, 19, -13, 19});
     private static final GuiModuleOffsets minorOffsets = new GuiModuleOffsets(new int[]{6, 1,-2, 4,2, 4});
     @ObjectHolder("tetra:modular_glove")
-    public static mod.captanredbeard.tetra_extra.items.modular.impl.ModularGloveItem instance;
+    public static ModularGloveItem instance;
 
     public ModularGloveItem() {
         super((new Properties()).maxStackSize(1).group(TetraItemGroup.instance).isImmuneToFire());
@@ -41,8 +32,8 @@ public class ModularGloveItem extends ItemModularHandheld {
         this.minorModuleKeys = new String[]{"glove/binding","glove/accessory","glove/digits"};
         this.requiredModules = new String[]{"glove/handle", "glove/head_left", "glove/head_right"};
         //this.updateConfig((Integer)ConfigHandler.honedoubleBase.get(), (Integer)ConfigHandler.honedoubleIntegrityMultiplier.get());
-        SchematicRegistry.instance.registerSchematic(new RepairSchematic(this));
-        RemoveSchematic.registerRemoveSchematics(this);
+ //       SchematicRegistry.instance.registerSchematic(new RepairSchematic(this));
+   //     RemoveSchematic.registerRemoveSchematics(this);
     }
 /*
     public void updateConfig(int honeBase, int honeIntegrityMultiplier) {
@@ -120,6 +111,7 @@ public class ModularGloveItem extends ItemModularHandheld {
         return minorOffsets;
     }
 
+/*
     public String getModelCacheKey(ItemStack itemStack, LivingEntity entity) {
         return entity != null && itemStack.equals(entity.getActiveItemStack()) ? (String)Optional.ofNullable(this.getChargeableAbility(itemStack)).map(ChargedAbilityEffect::getModelTransform).map((transform) -> {
             return super.getModelCacheKey(itemStack, entity) + ":" + transform;
@@ -127,7 +119,9 @@ public class ModularGloveItem extends ItemModularHandheld {
             return super.getModelCacheKey(itemStack, entity);
         }) : super.getModelCacheKey(itemStack, entity);
     }
-/*
+
+
+
     @OnlyIn(Dist.CLIENT)
     public String getTransformVariant(ItemStack itemStack, @Nullable LivingEntity entity) {
         ChargedAbilityEffect ability = this.getChargeableAbility(itemStack);
